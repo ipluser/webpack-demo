301b90462f754709c6a9032ec25e1bbc1deb0469
lefeng <hainan.qhn@alibaba-inc.com>
2016-07-05 09:43:12 +0800

    feature: init repository

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
+
+const parts = require('./libs/parts');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack'
+    })
+  ]
+};
+
+var config;
+
+switch (process.env.npm_lifecycle_event) {
+  case 'build':
+    config = merge(common, {});
+    break;
+  default:
+    config = merge(
+      common,
+      parts.devServer({
+        host: process.env.HOST,
+        port: process.env.PORT
+      })
+    );
+}
+
+module.exports = validate(config);
