1c267e2e2aed125a0ea59ea1c9009a1093301196
lefeng <hainan.qhn@alibaba-inc.com>
2016-07-08 13:29:23 +0800

    feat(hash): init chunkhash

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,14 @@ switch (process.env.npm_lifecycle_event) {
   case 'build':
     config = merge(
       common,
-      { devtool: 'source-map' },
+      {
+        devtool: 'source-map',
+        output: {
+          path: PATHS.build,
+          filename: '[name].[chunkhash].js',
+          chunkFilename: '[chunkhash].js'
+        }
+      },
       parts.setFreeVariables({
         'process.env.NODE_ENV': 'production'
       }),
